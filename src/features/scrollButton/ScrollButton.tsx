3a38d3be47b7f471015a07485ef9129bd3f638f4
Rodion <eclipselucky@gmail.com>
2023-06-13 23:10:39 +0700

    added welcome block headers and buttons

---
+++ src/features/scrollButton/ScrollButton.tsx
@@ -0,0 +1,23 @@
+import Button from "src/shared/UI/button/Button"
+
+interface IScrollButtonSettings {
+  type: "filledLight" | "Ghost" | "filledDark" | "notAllowedDark"
+  text: string
+  path: string
+}
+
+export const ScrollButton = ({ type, text, path }: IScrollButtonSettings) => {
+  const scrollTo = (path: string) => {
+    console.log({ path })
+  }
+
+  return (
+    <div>
+      <div onClick={() => scrollTo(path)}>
+        <Button type={type} text={text} />
+      </div>
+    </div>
+  )
+}
+
+export default ScrollButton
--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,11 +1,25 @@
+import ToggleModal from "src/features/toggleModal/ToggleModal"
 import styles from "./Home.module.scss"
-
+import ScrollButton from "src/features/scrollButton/scrollButton"
+import SignUpForm from "src/shared/UI/SignUpForm/SignUpForm"
 
 const Home: React.FC = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.welcomingWrapper}>
         <h1>Find Your Next Gig with Our Freelance Job Platform</h1>
+        <h3>
+          Streamline your freelance business with our job finder platform. Find
+          opportunities, manage workloads, and maximize earnings.
+        </h3>
+        <div className={styles.scrollButtons}>
+          <ToggleModal
+            type='filledLight'
+            text='Start free trial'
+            form={<SignUpForm />}
+          />
+          <ScrollButton text='Explore Denwa' type='filledDark' path='123' />
+        </div>
       </div>
     </div>
   )
