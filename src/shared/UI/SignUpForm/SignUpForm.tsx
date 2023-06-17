de182cdbeff010d5c5ce4cd4466309b4ac5dbcc1
Rodion <eclipselucky@gmail.com>
2023-06-17 15:01:40 +0700

    recreated forms

--- src/shared/UI/SignUpForm/SignUpForm.tsx
+++ src/shared/UI/SignUpForm/SignUpForm.tsx
@@ -1,17 +1,24 @@
 import Button from "../button/Button"
 import Input from "../input/Input"
-
+import { useForm, FormProvider } from "react-hook-form"
 export const SignUpForm = () => {
+   const methods = useForm()
+   const onSubmit = (data: any) => {
+      console.log(data)
+   }
+
    return (
-      <form>
-         <h1>Регистрация</h1>
-         <h2>Присоединяйтесь к нашей команде!</h2>
-         <p>Эл-почта:</p>
-         <Input type='text' placeholder='Эл-почта' name='email' />
-         <p>Пароль:</p>
-         <Input type='password' placeholder='Пароль' name='password' />
-         <Button text='Создать' type='filledDark' />
-      </form>
+      <FormProvider {...methods}>
+         <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <h1>Регистрация</h1>
+            <h2>Присоединяйтесь к нашей команде!</h2>
+            <p>Эл-почта:</p>
+            <Input type='text' placeholder='Эл-почта' name='email' />
+            <p>Пароль:</p>
+            <Input type='password' placeholder='Пароль' name='password' />
+            <Button text='Создать' type='filledDark' />
+         </form>
+      </FormProvider>
    )
 }
 export default SignUpForm
