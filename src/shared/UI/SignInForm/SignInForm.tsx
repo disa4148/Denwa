17c63f77b091e16259109475ed18a0133fdfed53
Rodion <eclipselucky@gmail.com>
2023-06-17 12:48:47 +0700

    react hook form installation commit

--- src/shared/UI/SignInForm/SignInForm.tsx
+++ src/shared/UI/SignInForm/SignInForm.tsx
@@ -1,19 +1,26 @@
-import Button from '../button/Button'
-import Input from '../input/Input'
+import Button from "../button/Button"
+import Input from "../input/Input"
+import { useForm, Controller } from "react-hook-form"
 
 
 export const SignInForm = () => {
-  return (
-    <form>
-        <h1>Авторизация</h1>
-        <h2>Получите доступ к своему профилю</h2>
-        <p>Эл-почта:</p>
-      <Input type='text' placeholder='Эл-почта'/>
-      <p>Пароль:</p>
-      <Input type='password' placeholder='Пароль'/>
-      <Button type='filledLight' text='Войти'/>
-    </form>
-  )
+   const { handleSubmit, register, reset, control } = useForm()
+   const onSubmit = (data: any) => {
+      console.log(data);
+   }
+
+   return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+         <h1>Авторизация</h1>
+         <h2>Получите доступ к своему профилю</h2>
+         <p>Эл-почта:</p>
+         <Controller name="email" control={control} render={() => <Input type='text' placeholder='Эл-почта' />}/>
+         
+         <p>Пароль:</p>
+         <Input type='password' placeholder='Пароль' />
+         <Button type='filledLight' text='Войти' />
+      </form>
+   )
 }
 
 export default SignInForm
